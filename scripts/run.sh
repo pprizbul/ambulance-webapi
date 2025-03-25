17b35a76b62cd605268ab041b59fc6b673a7fc62
Degrad21 <xprizbul@stuba.sk>
2025-03-25 15:22:15 +0100

    feat: add mongo database

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,13 +10,27 @@ ProjectRoot="$(dirname "$(realpath "$0")")/.."
 
 export AMBULANCE_API_ENVIRONMENT="Development"
 export AMBULANCE_API_PORT="8080"
+export AMBULANCE_API_MONGODB_USERNAME="root"
+export AMBULANCE_API_MONGODB_PASSWORD="neUhaDnes"
+
+mongo() {
+    docker compose --file "$ProjectRoot/deployments/docker-compose/compose.yaml" "$@"
+}
 
 case "$command" in
-    "start")
-        go run "$ProjectRoot/cmd/ambulance-api-service"
-        ;;
     "openapi")
         docker run --rm -ti -v "$ProjectRoot:/local" openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+        ;;
+    "start")
+        {
+            mongo up --detach
+            go run "$ProjectRoot/cmd/ambulance-api-service"
+        } || {
+            mongo down
+        }
+        ;;
+    "mongo")
+        mongo up
         ;;
     *)
         echo "Unknown command: $command" >&2
